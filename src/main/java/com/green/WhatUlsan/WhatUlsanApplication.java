f0ac45ce737bb31bb6bf091edc016f51834df5c7
HCJIN <ghkdckswls02@naver.com>
2024-08-27 16:00:24 +0900

    first

---
+++ src/main/java/com/green/WhatUlsan/WhatUlsanApplication.java
@@ -0,0 +1,13 @@
+package com.green.WhatUlsan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatUlsanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatUlsanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/green/WhatUlsan/WhatUlsanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.WhatUlsan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatUlsanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
